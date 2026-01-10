222320170921b0dc4ce7b02067a3553648749c52
Rahul2245 <rahulgs2024@gmail.com>
2026-01-11 01:10:40 +0530

    intiated webrtc datachannel and testing

--- webrtc-signal/server.js
+++ webrtc-signal/server.js
@@ -2,6 +2,8 @@ import { readFileSync } from 'fs'
 import { createServer } from 'https'
 import express from 'express'
 import { Server } from 'socket.io'
+import { dirname,join } from "node:path";
+import { fileURLToPath } from "node:url";
 
 
 
@@ -11,13 +13,18 @@ const app = express()
 const key = readFileSync('./cert/cert.key')
 const cert = readFileSync('./cert/cert.crt')    
 
-const secureExpressServer = createServer({key,cert,
-    requestCert :true,
-    ca:[
-        readFileSync('./cert/ca.crt')
-    ]
+const secureExpressServer = createServer({key,cert
+    // requestCert :true,
+    // ca:[
+    //     readFileSync('./cert/ca.crt')
+    // ]
 },app)
 const io = new Server(secureExpressServer);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.get('/',(req,res)=>{
+    res.sendFile(join(__dirname,'sample.html'));
+});
 
 io.on('connection',(socket)=>{
     console.log('a user entered the connection',socket.id);
@@ -48,11 +55,15 @@ io.on('connection',(socket)=>{
 
     //join room
     socket.on('join-room',(data)=>{
+        console.log('joined room',socket.id);
         const roomid = data.roomid;
+        console.log(roomid);
         socket.join(roomid);
         const clients = io.sockets.adapter.rooms.get(roomid);
+        console.log(clients.size);
         if(clients.size > 1){
             socket.to(roomid).emit('initiator');
+            console.log('more than 2 clients',socket.id);
         }
 
     })
